ed74641c76244160d9c2d9407f09663184b9aef3
Kilo Meow <dest@disr.it>
2021-09-06 15:57:33 +0300

    story widget lead

--- src/routes/widget.svelte
+++ src/routes/widget.svelte
@@ -4,4 +4,38 @@ import StoryWidget from '../components/StoryWidget.svelte'
 import { ydoc, swarm } from '../stores/room'
 
 </script>
-<StoryWidget />
+
+<style>
+
+body {
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    font-size: 16px;
+}
+
+.page {
+    max-width: 600px;
+    margin: auto;
+}
+
+.page h2 {
+    max-width: 60%;
+    margin: 25px 0;
+    font-size: 26px;
+    font-weight: 600;
+    line-height: 26px;
+}
+
+.page p {
+    margin: 20px 0;
+}
+
+</style>
+
+<div class="page">
+    <div>
+        <h2 w-60>Давайте соберём истории на удивительную тему</h2>
+        <p>У вас было такое в жизни? Вы или ваши близкие сталкивались с этим? Вы много об этом знаете? Расскажите свою историю.</p>
+    </div>
+
+    <StoryWidget />
+</div>
