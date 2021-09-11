a725f3fa03769b677dab6116963daf17f29cdb4e
Untone <anton.rewin@gmail.com>
2021-09-11 20:07:50 +0300

    minor fixes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,18 +1,14 @@
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
-const purgecss = require('@fullhuman/postcss-purgecss')
+// const purgecss = require('@fullhuman/postcss-purgecss')
 
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
+const plugins = [
+  tailwindcss(),
+  autoprefixer(),
+  // purgecss({ content: ['./src/**/*.svelte'] })
+]
 
-const config = {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-    purgecss({ content: ['./src/**/*.svelte'] }),
-    !dev && cssnano({ preset: 'default' })
-  ]
-}
+if(process.env.NODE_ENV !== 'development') plugins.push(cssnano({ preset: 'default' }))
 
-module.exports = config
+module.exports = { plugins}
