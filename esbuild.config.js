50a6c4d971ae4e9b9a6d94d7ec923efd0039fdf8
Untone <anton.rewin@gmail.com>
2021-11-02 00:09:32 +0300

    esbuild-svelte version fix

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,78 +1,73 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { build } from 'esbuild'
 import svelte from 'esbuild-svelte'
 import { derver } from 'derver'
-import { createRequire } from 'module'
-
-const require = createRequire(import.meta.url)
-const fs = require('fs')
-const { preprocess } = require('./svelte.config.cjs')
+import fs from 'fs'
 
 const dev = process.env.NODE_ENV != 'production'
-if(dev) console.info('[dev mode]')
 const port = 5000
 const dir = 'public'
 
-const options = {
-  globalName: 'discours',
-  format: 'iife',
-  platform: 'browser',
-  minifyIdentifiers: true,
-  entryPoints: [`src/index.ts`],
-  bundle: true,
-  splitting: false,
-  write: true,
-  allowOverwrite: true,
-  color: true,
-  incremental: dev,
-  outfile: dir + `/bundle.js`,
-  plugins: [
-    svelte({
-      compileOptions: {
-        dev,
-        css: true,
-        generate: 'dom',
-      },
-      preprocess,
-    }),
-  ]
-}
+import('./svelte.config.cjs').then(svelteConfig => {
 
-if (!dev) {
-  options.minify = true
-  options.treeShaking = true
-}
-
-build(options)
-  .then((builder) => {
-    if (builder.warnings && builder.warnings.length) {
-      builder.warnings.forEach((w) => console.warn(w))
-      return
-    }
-    if (dev) {
-      derver({
-        dir,
-        port,
-        watch: [dir, 'src'],
-        onwatch: async (lr, item) => {
-          if (item == 'src') {
-            lr.prevent()
-            try {
-              await builder.rebuild()
-            } catch (err) {
-              lr.error(err.message, 'Svelte compile error')
-            }
-          }
+  const options = {
+    globalName: 'aho',
+    format: 'iife',
+    platform: 'browser',
+    minifyIdentifiers: true,
+    entryPoints: [`src/index.ts`],
+    bundle: true,
+    splitting: false,
+    write: true,
+    allowOverwrite: true,
+    color: true,
+    incremental: dev,
+    outfile: dir + `/bundle.js`,
+    plugins: [
+      svelte({
+        compileOptions: {
+          dev,
+          css: true,
+          generate: 'dom',
         },
-      })
-    } else {
-      const stats = fs.statSync(options.outfile)
-      console.log(`bunbdle size: ${Math.round(stats.size/100)/10} Kb`)
-      console.log('esbuild: successfully built for production')
-      process.exit(0)
-    }
-  })
-  .catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
+        ...svelteConfig,
+      }),
+    ],
+  }
+  
+  if (!dev) {
+    options.minify = true
+    options.treeShaking = true
+  }
+  
+  build(options)
+    .then((builder) => {
+      if (builder.warnings && builder.warnings.length) {
+        builder.warnings.forEach((w) => console.warn(w))
+        return
+      }
+      if (dev) {
+        derver({
+          dir,
+          port,
+          watch: [dir, 'src'],
+          onwatch: async (lr, item) => {
+            if (item == 'src') {
+              lr.prevent()
+              try {
+                await builder.rebuild()
+              } catch (err) {
+                lr.error(err.message, 'Svelte compile error')
+              }
+            }
+          },
+        })
+      } else {
+        console.log(`bundle size: ${Math.round(fs.statSync(options.outfile).size/100)/10} Kb`)
+        process.exit(0)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      process.exit(1)
+    })
+})
--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,18 +1,14 @@
+let preprocess = []
 
-const { typescript } = require('svelte-preprocess-esbuild')
-const { windi: windiSvelte } = require('svelte-windicss-preprocess')
+import('svelte-windicss-preprocess').then(({ windi }) => preprocess.push(windi({
+  compile: false,
+  config: 'tailwind.config.cjs',
+  debug: true,
+  globalPreflight: true, // will be deprecated soon
+  globalUtility: true,
+  //prefix: 'windi-',
+  silent: false,
+})))
+import('svelte-preprocess-esbuild').then(({ typescript }) => preprocess.push(typescript()))
 
-module.exports = {
-    preprocess: [
-        windiSvelte({
-          compile: false,
-          config: 'tailwind.config.cjs',
-          debug: true,
-          globalPreflight: true, // will be deprecated soon
-          globalUtility: true,
-          //prefix: 'windi-',
-          silent: false,
-        }),
-        typescript(),
-      ],
-}
+module.exports = { preprocess }
