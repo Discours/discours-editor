d70e546c17e859be061e65d4af46a6b51cb31d57
Untone <anton.rewin@gmail.com>
2021-11-03 16:38:10 +0300

    fixes..

--- esbuild.config.js
+++ esbuild.config.js
@@ -5,23 +5,22 @@ import fs from 'fs'
 
 const dev = process.env.NODE_ENV != 'production'
 const port = 5000
-const dir = 'public'
+const dir = 'dist'
 
 import('./svelte.config.cjs').then(svelteConfig => {
 
   const options = {
     globalName: 'discours',
     format: 'iife',
-    platform: 'browser',
-    minifyIdentifiers: true,
-    entryPoints: [`src/Editor.svelte`],
+    // minifyIdentifiers: true,
+    entryPoints: [ 'src/Editor.svelte' ],
     bundle: true,
     splitting: false,
     write: true,
     allowOverwrite: true,
     color: true,
-    incremental: dev,
-    outfile: dir + `/bundle.js`,
+    // incremental: dev,
+    outfile: dir + '/index.js',
     plugins: [
       svelte({
         compileOptions: {
@@ -39,35 +38,38 @@ import('./svelte.config.cjs').then(svelteConfig => {
     options.treeShaking = true
   }
   
-  build(options)
-    .then((builder) => {
-      if (builder.warnings && builder.warnings.length) {
-        builder.warnings.forEach((w) => console.warn(w))
-        return
-      }
-      if (dev) {
-        derver({
-          dir,
-          port,
-          watch: [dir, 'src'],
-          onwatch: async (lr, item) => {
-            if (item == 'src') {
-              lr.prevent()
-              try {
-                await builder.rebuild()
-              } catch (err) {
-                lr.error(err.message, 'Svelte compile error')
-              }
-            }
-          },
+  build({ ...options, outfile: dir + '/index.esm.js', format: 'esm' })
+    .then((b) => {
+      build(options)
+        .then((builder) => {
+          console.log(`index.esm.js ${Math.round(fs.statSync(dir + '/index.esm.js').size/100)/10} Kb`)
+          console.log(`index.js ${Math.round(fs.statSync(dir + '/index.js').size/100)/10} Kb`)
+          if (builder.warnings && builder.warnings.length) {
+            builder.warnings.forEach((w) => console.warn(w))
+            return
+          }
+          if (dev) {
+            derver({
+              dir,
+              port,
+              watch: [dir, 'src'],
+              onwatch: async (lr, item) => {
+                if (item == 'src') {
+                  lr.prevent()
+                  try {
+                    await builder.rebuild()
+                  } catch (err) {
+                    lr.error(err.message, 'Svelte compile error')
+                  }
+                }
+              },
+            })
+          } else process.exit(0)
         })
-      } else {
-        console.log(`bundle size: ${Math.round(fs.statSync(options.outfile).size/100)/10} Kb`)
-        process.exit(0)
-      }
+        .catch((err) => {
+          console.error(err)
+          process.exit(1)
+        })
     })
-    .catch((err) => {
-      console.error(err)
-      process.exit(1)
-    })
-})
+  })
+  
