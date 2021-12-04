996c15dfd309e6cd1a91f4d8c3f3be8801b33e42
Tony Rewin <anton.rewin@gmail.com>
2021-12-04 17:55:58 +0300

    preprocessors added

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
 import adapter from '@sveltejs/adapter-auto'
 import { windi as windiSvelte } from 'svelte-windicss-preprocess'
 import { typescript } from 'svelte-preprocess-esbuild'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const { scss, globalStyle } = require('svelte-preprocess')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [windiSvelte(), typescript()],
+  preprocess: [windiSvelte(), typescript(), scss(), globalStyle()],
   router: false,
   kit: {
     adapter: adapter(),
