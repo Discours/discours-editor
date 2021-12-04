c6d8c5c019bfa52badb4f7a07902008120a3d498
Tony Rewin <anton.rewin@gmail.com>
2021-12-04 18:16:58 +0300

    +

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -49,4 +49,81 @@
   onDestroy(() => view.destroy())
 </script>
 
-<div transition:fade bind:this={master} />
+<div class="editor" transition:fade bind:this={master} />
+
+<style global>
+.ProseMirror {
+    position: relative;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+    -webkit-font-variant-ligatures: none;
+    font-variant-ligatures: none;
+    outline: none;
+  }
+
+  .ProseMirror pre {
+    white-space: pre-wrap;
+  }
+
+  .ProseMirror li {
+    position: relative;
+  }
+
+  .ProseMirror-hideselection *::selection {
+    background: transparent;
+  }
+
+  .ProseMirror-hideselection *::-moz-selection {
+    background: transparent;
+  }
+
+  .ProseMirror-hideselection {
+    caret-color: transparent;
+  }
+
+  .ProseMirror-selectednode {
+    outline: 2px solid #8cf;
+  }
+
+  /* Make sure li selections wrap around markers */
+
+  li.ProseMirror-selectednode {
+    outline: none;
+  }
+
+  li.ProseMirror-selectednode:after {
+    content: '';
+    position: absolute;
+    left: -32px;
+    right: -2px;
+    top: -2px;
+    bottom: -2px;
+      border: 2px solid #8cf;
+      pointer-events: none;
+    }
+  
+    .ProseMirror .empty-node::before {
+      position: absolute;
+      color: #aaa;
+      cursor: text;
+    }
+  
+    .ProseMirror .empty-node:hover::before {
+      color: #777;
+    }
+  
+    .ProseMirror.editor_empty::before {
+      position: absolute;
+      content: attr(data-placeholder);
+      pointer-events: none;
+      color: var(--ui-color-placeholder);
+    }
+  div.editor {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border: 1px lightgray solid;
+      border-radius: 3px;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,8 @@
 <script lang="ts">
   import Editor from '../components/Editor.svelte'
-  import P2P from '../components/P2P.svelte'
-  import { p2p, roompass, ydoc } from '../store'
-  import MD from 'markdown-it'
-  import footnotePlugin from 'markdown-it-footnote'
-  import markPlugin from 'markdown-it-mark'
-  // import containerPlugin from 'markdown-it-container'
-  const mit = new MD()
-    // mit.use(containerPlugin, name [, options])
-    .use(footnotePlugin)
-    .use(markPlugin)
+  import { p2p, ydoc } from '../store'
 </script>
 
-<div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md">
-  <P2P password={$roompass} />
-  {#if $p2p && $p2p.awareness}
-    <Editor ydoc={$ydoc} awareness={$p2p.awareness} />
-  {/if}
-</div>
-
-<style global>
-  div {
-    margin: 0;
-    padding: 0;
-  }
-</style>
+{#if $p2p && $p2p.awareness}
+  <Editor ydoc={$ydoc} awareness={$p2p.awareness} />
+{/if}
